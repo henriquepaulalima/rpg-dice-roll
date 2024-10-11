fa142b42931b1c5c3c0ab54cff6c7e3dbc9a5685
henriquepaulima <henrique.p.lima0@gmail.com>
2024-10-11 15:04:17 -0300

    Add new history and make it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,24 @@
 
 import styles from './page.module.scss';
 import { useEffect, useState } from 'react';
-import { Button } from '@mui/material';
+import {
+  Backdrop,
+  Box,
+  Button,
+  ButtonGroup,
+  Fade,
+  Modal,
+  Typography,
+} from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDice, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faDice, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   const [number, setNumber] = useState<number>(0);
   const [history, setHistory] = useState<number[]>([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     const historyValue = localStorage.getItem('history');
@@ -35,31 +46,66 @@ export default function Home() {
   const clearHistory = () => {
     setHistory([]);
     saveHistory();
+    handleClose();
   };
 
   return (
     <main className={styles.d100__container}>
       <div className={styles.d100__roll_history}>
-        <div className={styles.d100__roll_history_title}>
-          <h2>History</h2>
-          <Button
-            onClick={clearHistory}
-            disabled={history.length <= 0}
-            variant='contained'
-          >
-            Clear
-            <FontAwesomeIcon icon={faTrashCan} />
-          </Button>
-        </div>
-        <div className={styles.d100__roll_history_list}>
-          {history &&
-            history.map((item, index) => (
-              <div className={styles.d100__roll_history_item} key={index}>
-                <p>{item}</p>
-                {index < history.length - 1 && <p>{'>'}</p>}
+        <Button
+          onClick={handleOpen}
+          disabled={history.length <= 0}
+          variant='contained'
+        >
+          History
+        </Button>
+        <Modal
+          aria-labelledby='transition-modal-title'
+          aria-describedby='transition-modal-description'
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={open}>
+            <div className={styles.d100__roll_history_modal}>
+              <div className={styles.d100__roll_history_modal_title}>
+                <h2>History</h2>
+                <ButtonGroup
+                  variant='contained'
+                  aria-label='Basic button group'
+                >
+                  <Button
+                    onClick={clearHistory}
+                    disabled={history.length <= 0}
+                    color='error'
+                  >
+                    Clear
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </Button>
+                  <Button onClick={handleClose}>
+                    <FontAwesomeIcon icon={faClose} />
+                  </Button>
+                </ButtonGroup>
               </div>
-            ))}
-        </div>
+              <div className={styles.d100__roll_history_modal_list}>
+                {history.map((item, index) => (
+                  <div
+                    className={styles.d100__roll_history_modal_list_item}
+                    key={index}
+                  >
+                    <p>{item}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </Fade>
+        </Modal>
       </div>
       <div className={styles.d100__roll_container}>
         <p>{number}</p>
